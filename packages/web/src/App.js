51e8c76bbddd8ebaa45f5b6fb258756b3fc57a48
Dragos Cirjan <dragos.cirjan+github2@gmail.com>
2021-03-29 01:05:39 +0300

    +

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -2,23 +2,37 @@ import logo from './logo.svg';
 import "@fontsource/roboto";
 import './App.css';
 
+import {AppBar, Toolbar, Typography, Button, IconButton} from '@material-ui/core'
+import {MenuIcon} from '@material-ui/icons/MenuIcon'
+import {makeStyles} from '@material-ui/icons'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="static">
+  <Toolbar>
+    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+      <MenuIcon />
+    </IconButton>
+    <Typography variant="h6" className={classes.title}>
+      News
+    </Typography>
+    <Button color="inherit">Login</Button>
+  </Toolbar>
+</AppBar>
     </div>
   );
 }
