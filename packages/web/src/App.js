f32104a9dff4f5f57988bb6341ab77fc1bd63410
Dragos Cirjan <dragos.cirjan@gmail.com>
2021-03-29 01:46:05 +0300

    +

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
-import "@fontsource/roboto";
+// import "@fontsource/roboto";
 import './App.css';
 
 import {AppBar, Toolbar, Typography, Button, IconButton} from '@material-ui/core'
-import {MenuIcon} from '@material-ui/icons/MenuIcon'
-import {makeStyles} from '@material-ui/icons'
+import { makeStyles } from '@material-ui/core/styles';
+// import AppBar from '@material-ui/core/AppBar';
+// import Toolbar from '@material-ui/core/Toolbar';
+// import Typography from '@material-ui/core/Typography';
+// import Button from '@material-ui/core/Button';
+// import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -24,11 +29,11 @@ function App() {
     <div className="App">
       <AppBar position="static">
   <Toolbar>
-    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+    {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
       <MenuIcon />
-    </IconButton>
+    </IconButton> */}
     <Typography variant="h6" className={classes.title}>
-      News
+      My Flora
     </Typography>
     <Button color="inherit">Login</Button>
   </Toolbar>
