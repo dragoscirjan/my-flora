bb1b9719d004613d031fc1bbc8ed13898ad085bd
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2021-03-30 02:30:40 +0300

    +

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-// import "@fontsource/roboto";
-import './App.css';
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
-import {AppBar, Toolbar, Typography, Button, IconButton} from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles';
-// import AppBar from '@material-ui/core/AppBar';
-// import Toolbar from '@material-ui/core/Toolbar';
-// import Typography from '@material-ui/core/Typography';
-// import Button from '@material-ui/core/Button';
-// import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import "./App.css";
+
+import Logo from "./views/Logo";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
     flexGrow: 1,
   },
 }));
@@ -28,16 +19,14 @@ function App() {
   return (
     <div className="App">
       <AppBar position="static">
-  <Toolbar>
-    {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-      <MenuIcon />
-    </IconButton> */}
-    <Typography variant="h6" className={classes.title}>
-      My Flora
-    </Typography>
-    <Button color="inherit">Login</Button>
-  </Toolbar>
-</AppBar>
+        <Toolbar>
+          <Logo />
+          <Typography variant="h6" className={classes.title}>
+            My Flora
+          </Typography>
+          {/* <Button color="inherit">Login</Button> */}
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
